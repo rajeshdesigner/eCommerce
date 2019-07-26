d20962ee5f71306eb0cbab5231ffed1c66aff284
rajeshdesigner <mail2rajesh.t@gmail.com>
2019-07-26 22:22:51 +0530

    Update our menu-items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,29 +11,34 @@ class Directory extends Component {
                 {
                     title: 'HATS',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hat.png',
-                    id: 1
+                    id: 1,
+                    linkUrl: 'hats'
                 },
                 {
                     title: 'JACKETS',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jacket.png',
-                    id: 2
+                    id: 2,
+                    linkUrl: ''
                 },
                 {
                     title: 'SNEAKERS',
                     imageUrl: 'https://i.ibb.co/0jqHpnp/sneaker.png',
-                    id: 3
+                    id: 3,
+                    linkUrl: ''
                 },
                 {
                     title: 'WOMENS',
                     imageUrl: 'https://i.ibb.co/GCCdy8t/women.png',
                     size: 'large',
-                    id: 4
+                    id: 4,
+                    linkUrl: ''
                 },
                 {
                     title: 'MENS',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
-                    id: 5
+                    id: 5,
+                    linkUrl: ''
                 }
             ]
         }
@@ -42,8 +47,8 @@ class Directory extends Component {
     render() {
         return (
             <div className='directory-menu'>
-                {this.state.sections.map(({ title, imageUrl, id, size }) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                {this.state.sections.map(({ id, ...otherSectionProps  }) => (
+                        <MenuItem key={id} {...otherSectionProps} />
                     ))}
             </div>
         )
